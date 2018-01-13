8bb920b4c33ce231272b2dc88b0136dee4360208
Kevin <lhs168@gmail.com>
2018-01-13 19:42:44 +0800

    分页加强

--- src/Fasim/Library/Pager.php
+++ src/Fasim/Library/Pager.php
@@ -10,13 +10,14 @@ class Pager {
 	const Normal = 1;
 	const Simple = 2;
 	const Bootstrap = 3;
+	const Popular = 4;
 
 	public $text = '&laquo;,&lsaquo;,&rsaquo;,&raquo;';
 	public $tip = '&laquo;,&lsaquo;,&rsaquo;,&raquo;';
 	public $showPage = 10;
 	public $totalPage = 0;
 	public $url = '?page={page}';
-	public $style = self::Normal;
+	public $style = self::Popular;
 	public $offset = 0;
 
 	private $page = 1;
@@ -52,43 +53,65 @@ class Pager {
 		//计算起始和结束位置
 		if ($this->totalPage < 1) $this->totalPage = 1;
 		if ($this->showPage > $this->totalPage) $this->showPage = $this->totalPage;
-		if ($this->page <= intval(($this->showPage) / 2)) {
+		$s = $this->page - ceil($this->showPage / 2.0) + 1;
+		if ($s < 1) {
 			$s = 1;
-			$e = $this->showPage;
-		}else if ($this->totalPage-$this->page<=intval($this->showPage / 2)) {
-			$e = $this->totalPage;
-			$s = $this->totalPage-$this->showPage + 1;
-		}else{
-			$s = $this->page-intval(($this->showPage + 1)/2)+1;
-			$e = $s + $this->showPage-1;
 		}
+		$e = $s + $this->showPage - 1;
+		if ($e > $this->totalPage) {
+			$s = $this->totalPage - $this->showPage + 1;
+			$e = $this->totalPage;
+		}
+
 		//分页
 		$texts = explode(',', $this->text);
 		$tips = explode(',', $this->tip);
 		if (count($texts) != 4)  return;
 
 		$showExt = true;
-		if ($this->style == self::Simple) {
+		if ($this->style == self::Simple || $this->style == self::Popular) {
 			$showExt = false;
 		}
 
 		$html = '';
-		//第一页
-		if (($this->page > intval(($this->showPage + 1) / 2) && $this->showPage < $this->totalPage) || ($showExt && $this->page != 1)) {
-			$toPage = '1';
-			$html .= "<a href=\"".str_replace('{page}', '1', $this->url)."\" title=\"{$tips[0]}\">{$texts[0]}</a>";
-		} else if ($showExt) {
-			$html .= "<span title=\"{$tips[0]}\">{$texts[0]}</span>";
+		if ($this->style != self::Popular) {
+			//第一页
+			if ($showExt) {
+				if ($s != 1) {
+					$toPage = '1';
+					$html .= "<a href=\"".str_replace('{page}', '1', $this->url)."\" title=\"{$tips[0]}\">{$texts[0]}</a>";
+				} else  {
+					$html .= "<span title=\"{$tips[0]}\">{$texts[0]}</span>";
+				}
+			}
 		}
 		//上一页
+		//$e2 = $e;
 		if ($this->page > 1) {
 			$toPage = $this->page - 1;
 			$html .= "<a href=\"".str_replace('{page}', $toPage, $this->url)."\" rel=\"nofollow\" title=\"{$tips[1]}\">{$texts[1]}</a>";
 		} else if ($showExt) {
-			$html .= "<span title=\"{$tips[1]}\">{$texts[1]}</span>";
+			$html .= "<span title=\"{$tips[1]}\">…</span>";
 		}
 		//列页
-		for($p = $s; $p <= $e; $p++) {
+		if ($this->style == self::Popular && $s != 1) {
+			$html .= "<a href=\"".str_replace('{page}', 1, $this->url)."\" rel=\"nofollow\">1</a>";
+			$s++;
+			if ($s != 2) {
+				$html .= "<span>…</span>";
+				$s++;
+			}
+		}
+		$end = '';
+		if ($this->style == self::Popular && $e != $this->totalPage) {
+			$end .= "<a href=\"".str_replace('{page}', $this->totalPage, $this->url)."\" rel=\"nofollow\">{$this->totalPage}</a>";
+			$e--;
+			if ($e < $this->totalPage - 1) {
+				$end = "<span>…</span>" . $end ;
+				$e--;
+			}
+		}
+		for ($p = $s; $p <= $e; $p++) {
 			$toPage = $p;
 			if ($p == $this->page) {
 				$html .= "<strong>".$p."</strong>";
@@ -96,6 +119,8 @@ class Pager {
 				$html .= "<a href=\"".str_replace('{page}', $toPage, $this->url)."\" rel=\"nofollow\">".$p."</a>";
 			}
 		}
+		$html .= $end;
+
 		//下一页
 		if ($this->page < $this->totalPage) {
 			$toPage = $this->page + 1;
@@ -103,12 +128,16 @@ class Pager {
 		} else if ($showExt) {
 			$html .= "<span title=\"{$tips[2]}\">{$texts[2]}</span>";
 		}
-		//最后页
-		if (($this->page <= intval(($this->showPage + 1)/2) && $this->showPage < $this->totalPage) || ($showExt && $this->page != $this->totalPage)) {
-			$toPage = $this->totalPage;
-			$html .= "<a href=\"".str_replace('{page}', $toPage, $this->url)."\" rel=\"nofollow\" title=\"{$tips[3]}\">{$texts[3]}</a>";
-		} else if ($showExt) {
-			$html .= "<span title=\"{$tips[3]}\">{$texts[3]}</span>";
+		if ($this->style != self::Popular) {
+			//最后页
+			if ($showExt) {
+				if ($e != $this->totalPage) {
+					$toPage = $this->totalPage;
+					$html .= "<a href=\"".str_replace('{page}', $toPage, $this->url)."\" rel=\"nofollow\" title=\"{$tips[3]}\">{$texts[3]}</a>";
+				} else if ($showExt) {
+					$html .= "<span title=\"{$tips[3]}\">{$texts[3]}</span>";
+				}
+			}
 		}
 
 		if ($this->style == self::Bootstrap) {
